4ac12e54c89dfa0c8f112b4814b6cdfd81ff70d5
Anish Kumar <kumaranish200a@gmail.com>
2023-11-05 21:20:40 +0530

    intital commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const hh=document.querySelector('.hour');
+const min=document.querySelector('.min');
+const sec=document.querySelector('.sec');
+
+
+const setcloc=()=>
+{
+    let day=new Date;
+
+    let h=day.getHours()*30;
+    let m=day.getMinutes()*6;
+    let s=day.getSeconds()*6;
+    console.log(h,m,s);
+
+    hh.style.transform=`rotateZ(${(h)}deg)`;
+    min.style.transform=`rotateZ(${m}deg)`;
+    sec.style.transform=`rotateZ(${s}deg)`;
+}
+setcloc();
+
+setInterval(setcloc,1000);   
+
+
+const btn = document.querySelector('.btn');
+let isWhiteBackground = true;
+const clock=document.querySelector('.clock');
+
+const changebg = () => {
+    if (isWhiteBackground) {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        clock.style.boxShadow = '0 0 20px 5px rgba(255, 255, 255, 0.7)';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        setTimeout(()=>
+        {
+            clock.style.boxShadow = 'none';
+        },3000);
+    }
+    isWhiteBackground = !isWhiteBackground; // Toggle the background color state
+}
+
+btn.addEventListener('click', changebg);
